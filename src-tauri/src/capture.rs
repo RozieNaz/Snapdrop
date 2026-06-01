448ccc1a7dc495e1478dc3626d76ffe5c0fa3653
Rozie Naz <rozes.crafts@gmail.com>
2026-06-01 18:49:22 +0100

    Add screenshot capture skeleton

---
+++ src-tauri/src/capture.rs
@@ -0,0 +1,11 @@
+pub struct ScreenshotService;
+
+impl ScreenshotService {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn capture_fullscreen(&self) {
+        // Windows capture implementation to be added.
+    }
+}
