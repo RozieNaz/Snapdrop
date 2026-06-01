1fa8462009d266b5b66b321ab070b002a9539e49
Rozie Naz <rozes.crafts@gmail.com>
2026-06-01 18:43:46 +0100

    Add tray menu module

---
+++ src-tauri/src/tray.rs
@@ -0,0 +1,25 @@
+use tauri::menu::{Menu, MenuItem};
+use tauri::tray::TrayIconBuilder;
+use tauri::{App, Manager};
+
+pub fn setup_tray(app: &mut App) -> tauri::Result<()> {
+    let open = MenuItem::with_id(app, "open", "Open Snapdrop", true, None::<&str>)?;
+    let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+    let menu = Menu::with_items(app, &[&open, &quit])?;
+
+    TrayIconBuilder::new()
+        .menu(&menu)
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "open" => {
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.show();
+                    let _ = window.set_focus();
+                }
+            }
+            "quit" => app.exit(0),
+            _ => {}
+        })
+        .build(app)?;
+
+    Ok(())
+}
